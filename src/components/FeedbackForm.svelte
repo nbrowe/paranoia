7f6250bcf387619e68cf064e3164fbf4a41ee756
Nathaniel Rowe <naterogen@outlook.com>
2022-10-01 00:46:31 -0400

    make feedback submittable

--- src/components/FeedbackForm.svelte
+++ src/components/FeedbackForm.svelte
@@ -1,11 +1,16 @@
 <script>
+  import {v4 as uuidv4} from 'uuid';
   import Button from "../ui/Button.svelte";
   import Card from "../ui/Card.svelte";
+    import RatingSelect from "./RatingSelect.svelte";
 
   let text = "";
+  let rating = 10;
   let btnDisabled = true;
   let min = 10;
   let message;
+
+  const handleSelect = (e) => rating = e.detail;
 
   const handleInput = () => {
     console.log(text.trim().length);
@@ -17,14 +22,27 @@
       btnDisabled = false;
     }
   };
+
+const handleSubmit = () => {
+  if(text.trim().length >= min) {
+    const newFeedback = {
+      id: uuidv4(),
+      text,
+      rating: +rating,
+    }
+
+    console.log(newFeedback);
+  }
+}
+
 </script>
 
 <Card>
   <header>
     <h2>How would you rate your service with us?</h2>
   </header>
-  <form>
-    <!-- Rating Select -->
+  <form on:submit|preventDefault={handleSubmit}>
+    <RatingSelect on:rating-select={handleSelect}/>
     <div class="input-group">
       <input
         type="text"
